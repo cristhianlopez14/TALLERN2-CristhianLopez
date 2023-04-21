8c3c9a00bb89a96ce9635ffe65b6670161f2d88a
cristhianlopez14 <cristhianl0306@gmail.com>
2023-04-20 19:50:39 -0500

    Actualizacion del index y creacion del evento click en el form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-function crearAlerta() {
-    alert('Hello World');
-}
+const form = document.getElementById('form') as HTMLFormElement;
+const btnGuardar = document.getElementById('btn-guardar') as HTMLButtonElement;
+
+btnGuardar.addEventListener('click', (e) =>{
+    e.preventDefault();
+    console.log('Ingreso al form');
+    
+
+    //Validacion del formulario
+
+
+    //Obtener valores de los inputs
+    const tipoDocumento = document.getElementById('tipoDocumento') as HTMLInputElement;
+    const numeroDocumento = document.getElementById('numeroDocumento') as HTMLInputElement;
+    const nombre = document.getElementById('nombre') as HTMLInputElement;
+    const apellido = document.getElementById('apellido') as HTMLInputElement;
+    const celular = document.getElementById('celular') as HTMLInputElement;
+    const correo = document.getElementById('correo') as HTMLInputElement;
+    const linkedin = document.getElementById('linkedin') as HTMLInputElement;
+    const github = document.getElementById('github') as HTMLInputElement;
+
+    console.log(nombre.value);
+    
+});
